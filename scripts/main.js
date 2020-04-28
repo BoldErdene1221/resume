23f52950f9f4dc2ae30b486507d645b7d98a09a5
BoldErdene1221 <azu.wert@gmail.com>
2020-04-28 10:03:33 +0800

    js changed

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,6 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
-
 $(document).ready(function() {
   AOS.init( {
-    // uncomment below for on-scroll animations to played only once
-    // once: true  
-  }); // initialize animate on scroll library
+  });
 });
 
 // Smooth scroll for links with hashes
